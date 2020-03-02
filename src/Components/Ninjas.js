04e19498ef8610caa0b62abb71550a651e0385d5
kel <ogbukakelechi20@gmail.com>
2020-03-02 09:15:25 +0100

    embedding our logic within the return

--- src/Components/Ninjas.js
+++ src/Components/Ninjas.js
@@ -3,23 +3,24 @@ import React, { Component } from 'react';
 // ui component (turned into a functional component because it does not have a state)
 const Ninja = ({ninjas}) =>{
   
-  if conditionals
-    const ninjaList = ninjas.map(ninja => {
-      if(ninja.age > 20){
-      return (
-        <div className="App" key = {ninja.id}>
-          {/* <h1>hello, i'm a Ninja</h1>; */}
-          <p>name: {ninja.name}</p>
-          <p>age: {ninja.age}</p>
-        </div>
-      )
-      } else{
-        return null
-      }
-    })
+  // if conditionals
+    // const ninjaList = ninjas.map(ninja => {
+    //   if(ninja.age > 20){
+    //   return (
+    //     <div className="App" key = {ninja.id}>
+    //       {/* <h1>hello, i'm a Ninja</h1>; */}
+    //       <p>name: {ninja.name}</p>
+    //       <p>age: {ninja.age}</p>
+    //     </div>
+    //   )
+    //   } else{
+    //     return null
+    //   }
+    // })
 
     // tenary conditionals
-    const ninjaList = ninjas.map(ninja => {
+ return (
+   ninjas.map(ninja => {
      return  ninja.age > 20 ? (
         <div className="App" key = {ninja.id}>
            {/* <h1>hello, i'm a Ninja</h1>; */}
@@ -28,10 +29,7 @@ const Ninja = ({ninjas}) =>{
         </div>
       )  : null
     })
-    return (
-        <div className = "ninja-list">
-           {ninjaList}
-        </div>
+   
     )
   
 }
